bfff590ce3915ee40d9eae9dbf88b10bd2e7b926
Scott Silverman <scottms724@gmail.com>
2019-04-30 18:01:36 +0000

    Automatically backed up by Learn

--- application.rb
+++ application.rb
@@ -2,7 +2,7 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, my name is Scott."
+    resp.write "Hello, World. My name is Scott."
     resp.finish
   end
 
